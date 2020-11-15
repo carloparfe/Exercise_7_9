198471dbfa3c152ad8649c5d1e0b14db8cd3eb78
Carlos Paredes <carlos.parfe@gmail.com>
2020-11-15 10:50:40 +0100

    Changes to make the code cleaner in the "digits.cs" file

--- digits.cs
+++ digits.cs
@@ -1,35 +1,52 @@
+/*
+Carlos Paredes Fernández - 1ºDAM
+Calculates how many numbers of 1, 2, 3 or more digits are entered by the user 
+until he/she types “end”.
+*/
+
 using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        int counterOneDigit = 0, counterTwoDigits = 0, counterThreeDigits = 0,
+            counterMoreDigits = 0;
+        
+        Console.Write("Insert a number: ");
+        // read the number as string to check the word "end"
+        string input = Console.ReadLine();
+        
+        while(input != "end")
+        {
+            int number = Convert.ToInt32(input);
+            
+            if(number != 0)
+            {
+                if(number / 10 == 0)
+                {
+                counterOneDigit++;
+                }
+                else if (number / 100 == 0)
+                {
+                counterTwoDigits++;
+                }
+                else if (number / 1000 == 0)
+                {
+                counterThreeDigits++;
+                }
+                else
+                {
+                counterMoreDigits++;
+                }
+            }
+            
+            input = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+        
+        Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", 
+            counterOneDigit, counterTwoDigits, counterThreeDigits, 
+            counterMoreDigits); 
+    }
 }
 
